3a6b155cce00ed614f46ac212e1cf69aaeed718c
Patrick Sevat <p.m.c.sevat@gmail.com>
2019-10-14 22:59:05 +0200

    fix(): linting and script fixes

--- src/component-compiler/stencil/packages/components/src/input-email/test/input-email.a11y.spec.ts
+++ src/component-compiler/stencil/packages/components/src/input-email/test/input-email.a11y.spec.ts
@@ -6,16 +6,16 @@ describe('input-email a11y', () => {
   });
 
   it('should be accessible', async () => {
-      await browser.execute(axe.source)
+      await browser.execute(axe.source);
 
       // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md
       const options = { runOnly: ['wcag2a', 'wcag2aa'] };
       // run inside browser and get results
-      const results = await browser.executeAsync((options, done) => {
-        const axe = (window as any).axe;
-        axe.run(document, options, function(err, results) {
+      const results = await browser.executeAsync((opts, done) => {
+        const globalAxe = (window as any).axe;
+        globalAxe.run(document, opts, (err, res) => {
           if (err) throw err;
-          done(results);
+          done(res);
         });
       }, options);
 
@@ -24,10 +24,10 @@ describe('input-email a11y', () => {
           const { html, failureSummary } = violation.nodes[0];
           return [...aggregator, { html, failureSummary }];
         }, []);
-        console.log(JSON.stringify(failureNodes, null, 2))
+        console.log(JSON.stringify(failureNodes, null, 2));
         fail();
       }
 
       expect(results.violations.length).toBe(0);
-  })
+  });
 });
